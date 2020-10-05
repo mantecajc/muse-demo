d0182f4f23676d4e82157b568cea74643aaac64c
Javier <65590523+javierc435@users.noreply.github.com>
2020-10-05 14:03:57 +0200

    Add and setup voting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,6 +42,18 @@ class PostsController < ApplicationController
     redirect_to root_path
   end
 
+  def upvote
+    @post = Post.find(params[:id])
+    @post.upvote_by current_user
+    redirect_to @post
+  end
+
+  def downvote
+    @post = Post.find(params[:id])
+    @post.downvote_by current_user
+    redirect_to @post
+  end
+
   private
 
   def find_post
